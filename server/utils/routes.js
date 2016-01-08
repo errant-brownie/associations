d828b05ed1fc09974ca447f866cc6cfb094c1bda
Alexander Wu <alexanderwu@Alexanders-MacBook-Pro.local>
2016-01-07 16:02:03 -0800

    Initial routes set up

--- server/utils/routes.js
+++ server/utils/routes.js
@@ -4,6 +4,14 @@ var auth = require('./auth'); // ./auth does some stuff to set up passport
 module.exports = function (app, express) {
   // put routes in here
 
+  // inputs:
+  // in data field:
+  //    user: 
+  //      username: the useraname
+  //      password: the password
+  // output:
+  // in data field:
+  //    message: if failure, reason for failure
   app.post('/api/users/signin', auth.authenticate);
   app.post('/api/users/signup', auth.createUser)
 
@@ -16,7 +24,7 @@ module.exports = function (app, express) {
   // outputs:
   // in data field:
   //   items: list of item objects that the user likes
-  app.get('/api/items', auth.ensureLoggedIn, itemController.getAll);
+  app.get('/api/items', itemController.getAll);
 
   // POST /api/items adds items the user likes
   // inputs: 
@@ -26,11 +34,34 @@ module.exports = function (app, express) {
   // outputs:
   // in data field;
   //   item: the new items that were created
-  app.post('/api/items', auth.ensureLoggedIn, itemsController.add);
+  app.post('/api/items', itemsController.add);
 
   // GET /api/associations gets the top items a user might like
-  // inputs in datafielt
-  app.get ('/api/associations', auth.ensureLoggedIn, associationController.getAssociations)
+  // inputs: 
+  // in data field:
+  //   nothing
+  // outputs:
+  // in data field:
+  //   items: list of objects, each object will have a property "item"
+  //     and "strength". "item" will link to an item object,
+  //     "strength" will link to the item's association strength
+  app.get ('/api/associations', associationController.getAssociations)
 
   // POST /api/associations custom association query
+  // inputs:
+  // in data field:
+  //   items: (optional) [array] of items to check for associations, 
+  //     if missing, association engine will be given logged in user's 
+  //     interest list
+  //   maxItems: [integer] maximum number of items to return
+  //   minStrength: [float between 0 and 1] minimum association strength
+  //   maxStrength: [float between 0 and 1] maximum association strength
+  //   myList: [boolean] wether or not to include logged in user's list
+  //     of interests. Ignored if no items were passed
+  // outputs:
+  // in data field:
+  //   items: list of objects, each object will have a property "item"
+  //     and "strength". "item" will link to an item object,
+  //     "strength" will link to the item's association strength
+  app.post('/api/associations', associationsController.postAssociations)
 };
