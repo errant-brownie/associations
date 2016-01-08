279770a8e903446d012234a4b420e27b438de85f
Alexander Wu <alexanderwu@Alexanders-MacBook-Pro.local>
2016-01-07 19:24:43 -0800

    items and association controllers return stub data

--- server/controllers/itemsController.js
+++ server/controllers/itemsController.js
@@ -1,1 +1,14 @@
-// item controller
+module.exports = {
+  getAll: function (request, response, next){
+    response.json([
+      {name: "video games"},
+      {name: "cats"},
+      {name: "computers"},
+      {name: "ui design"},
+    ]);
+  },
+
+  add: function(request, response, next){
+    respones.json(request.body);
+  }
+}
